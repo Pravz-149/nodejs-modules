398b6255bcc118db630d3f26e9cc7cf7fffe1fd2
Pravallika <100998718+Pravz-149@users.noreply.github.com>
2023-06-09 08:41:23 +0000

    Exported math.js module and imported it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs-modules
+
+A Pull Request is a feature provided by code hosting platforms like GitHub, GitLab, or Bitbucket. It is a way to propose changes to a codebase managed by version control systems like Git.
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+let add = require("./math.js");
+
+
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-let result = num1 + num2;
+let result = add(num1,num2);
 console.log(`Sum of numbers ${num1} and ${num2} is ${result}`);
